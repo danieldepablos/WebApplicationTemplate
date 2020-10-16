1bc814b42ee1318c56cfccd4c409ad5d11ee80b1
Daniel Depablos <ddepabloss@gmail.com>
2020-10-15 21:14:14 -0400

    Implementando un Custom Error Page

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index(int error = 0)
+        {
+            switch (error)
+            {
+                case 505:
+                    ViewBag.Title = "Ocurrio un error inesperado";
+                    ViewBag.Description = "Todos los detalles han sido registrados.";
+                    break;
+
+                case 404:
+                    ViewBag.Title = "Página no encontrada";
+                    ViewBag.Description = "La URL que Ud. está intentando ingresar no existe.";
+                    break;
+
+                default:
+                    ViewBag.Title = "Página no encontrada";
+                    ViewBag.Description = "Lo sentimos, esta página parece no existir. :( ..";
+                    break;
+            }
+
+            return View("~/views/helper/_ErrorPage.cshtml");
+
+        }
+    }
+}
--- changelog.md
+++ changelog.md
@@ -1,12 +1,16 @@
 ﻿# WebApplication Master Template
 > Plantilla Principal para Proyectos ASP.NET-MVC que incluye recientes versiones de dependencias.
 
-# Feactures
+# Dependencias
 
   - Entity Framework 6.4.4
   - Newton.Json 12.0.3
   - Bootstrap 4.5.3
   - JQuery 3.5.1
   - Bootstrap.Datepicker 1.8.0.1
+
+# Features
+
+  - Implementando un Custom Error Page
 
 Octubre 2020
